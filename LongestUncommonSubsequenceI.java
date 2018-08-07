0eb51884d728b8e271d70d5e12fb37e4bc8ad5a5
JoanaPeixoto <joana.saraiva.peixoto@gmail.com>
2018-08-07 21:01:17 +0100

    Add solution for LongestUncommonSubstringSequenceI

---
+++ LongestUncommonSubsequenceI.java
@@ -0,0 +1,9 @@
+class LongestUncommonSubsequenceI {
+
+  public int findLUSlength(String a, String b) {
+    if (a.equals(b)) {
+      return -1;
+    }
+    return Math.max(a.length(), b.length());
+  }
+}
